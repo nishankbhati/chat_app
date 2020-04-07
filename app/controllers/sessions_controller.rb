cc0fbf5fb5d0c5298e1aa30ed142ee1485a0335e
Nishank Bhati <nishank123xyz@gmail.com>
2020-04-08 00:35:32 +0530

    Logged in user restricted at controller level

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
 
+  before_action :logged_in_redirect, only: [:new, :create]
+
   def new
   end
 
   def create
-    user = User.find_by(username: params[:session][:username])
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(user_name: params[:session][:username])
+    validate_password = user.password_digest == params[:session][:password]
+    if user && validate_password
       session[:user_id] = user.id
       flash[:success] = "You have successfully logged in"
       redirect_to root_path
@@ -19,6 +22,15 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = "You have successfully logged out"
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
   end
 
 end
